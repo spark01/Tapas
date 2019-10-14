88f0eedd9d8db6459d86b11ab3a0234080195293
spark01 <btapas28@gmail.com>
2019-10-14 22:40:23 +0600

    CalculatorApp

---
+++ CalculatorAppPractice2/CalculatorAppPractice2/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAppPractice2
+{
+    class Calculator
+    {
+        public int FirstNumber;
+        public int SeceoandNumber;
+        public float Result;
+        
+        public float Add( )
+        {
+            Result = FirstNumber + SeceoandNumber;
+            return Result;
+        }
+        public float Sub()
+        {
+            Result = FirstNumber - SeceoandNumber;
+            return Result;
+        }
+        public float Mul()
+        {
+            Result = FirstNumber * SeceoandNumber;
+            return Result;
+        }
+        public float Div()
+        {
+            Result = FirstNumber / SeceoandNumber;
+            return Result;
+        }
+    }
+    
+}
---
+++ CalculatorAppPractice2/CalculatorAppPractice2/CalculatorUi.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorAppPractice2
+{
+    public partial class CalculatorUi : Form
+    {
+        Calculator calculator = new Calculator();
+        private double reg;
+        public CalculatorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            calculator.FirstNumber = Convert.ToInt32(firstNumberTextBox.Text);
+            calculator.SeceoandNumber = Convert.ToInt32(secondNumberTextBox.Text);
+            secondNumberTextBox.Clear();
+            firstNumberTextBox.Clear();
+
+           
+            resultTextBox.Text = calculator.Add( ).ToString();
+            
+
+        }
+
+        private void SubtractButton_Click(object sender, EventArgs e)
+        {
+            calculator.FirstNumber = Convert.ToInt32(firstNumberTextBox.Text);
+            calculator.SeceoandNumber = Convert.ToInt32(secondNumberTextBox.Text);
+            secondNumberTextBox.Clear();
+            firstNumberTextBox.Clear();
+            resultTextBox.Text = calculator.Sub().ToString();
+        }
+
+        private void MultiplyButton_Click(object sender, EventArgs e)
+        {
+            calculator.FirstNumber = Convert.ToInt32(firstNumberTextBox.Text);
+            calculator.SeceoandNumber = Convert.ToInt32(secondNumberTextBox.Text);
+            secondNumberTextBox.Clear();
+            firstNumberTextBox.Clear();
+            resultTextBox.Text = calculator.Mul().ToString();
+
+        }
+
+        private void DivideButton_Click(object sender, EventArgs e)
+        {
+            calculator.FirstNumber = Convert.ToInt32(firstNumberTextBox.Text);
+            calculator.SeceoandNumber = Convert.ToInt32(secondNumberTextBox.Text);
+            secondNumberTextBox.Clear();
+            firstNumberTextBox.Clear();
+            resultTextBox.Text = calculator.Div().ToString();
+
+        }
+    }
+}
