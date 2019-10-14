e855972e798fe9cdf9acef90e7187ffb99a98de4
spark01 <btapas28@gmail.com>
2019-10-14 22:46:30 +0600

    SalaryApp

---
+++ SalaryCalculatorAppPractice3/SalaryCalculatorAppPractice3/SalaryCalculatorUi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorAppPractice3
+{
+    public partial class SalaryCalculatorUi : Form
+    { 
+        SalaryCount salaryCount= new SalaryCount();
+
+        public SalaryCalculatorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowMeSalaryButton_Click(object sender, EventArgs e)
+        {
+
+            salaryCount.employeeName = employeeNameTextBox.Text;
+            salaryCount.basicAmount = Convert.ToDouble(basicAmountTextBox.Text);
+            salaryCount.houseRent = Convert.ToDouble(homeRentTextBox.Text);
+            salaryCount.madicaleAllowance = Convert.ToDouble(medicalAllowanceTextBox.Text);
+
+           salaryCount.Show();
+        }
+
+        private void ShowMeSalaryButton_Click_1(object sender, EventArgs e)
+        {
+            salaryCount.employeeName = employeeNameTextBox.Text;
+            salaryCount.basicAmount = Convert.ToDouble(basicAmountTextBox.Text);
+            salaryCount.houseRent = Convert.ToDouble(homeRentTextBox.Text);
+            salaryCount.madicaleAllowance = Convert.ToDouble(medicalAllowanceTextBox.Text);
+
+            salaryCount.Show();
+        }
+    }
+}
+
---
+++ SalaryCalculatorAppPractice3/SalaryCalculatorAppPractice3/SalaryCount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorAppPractice3
+{
+    class SalaryCount
+    {
+        public string employeeName;
+        public double basicAmount;
+        public double houseRent;
+        public double madicaleAllowance;
+        public double salary;
+       
+
+        public double HouseRent()
+        {
+            salary = (basicAmount * houseRent / 100);
+            return salary;
+        }
+
+        public double MadicaleAllowance()
+        {
+            salary = (basicAmount * houseRent / 100);
+            return salary;
+        }
+
+        public void Show()
+        {
+            string message;
+            message = "Name :" + employeeName + "\n" + "Salary" + salary + "\n";
+        }
+
+    }
+}
