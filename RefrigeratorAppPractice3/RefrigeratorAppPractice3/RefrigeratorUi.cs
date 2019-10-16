36382b30eca4331089fe4192ac7f4b4497cf5dc6
spark01 <btapas28@gmail.com>
2019-10-16 11:17:41 +0600

    assaignment12

---
+++ RefrigeratorAppPractice3/RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+        Refrigeretor aRefrigeretor = new Refrigeretor();
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            aRefrigeretor.AddWeight(Convert.ToDouble(maxWeightTakeTextBox.Text));
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            aRefrigeretor.AddItemUnit(Convert.ToDouble(itemTextBox.Text), Convert.ToDouble(weightTextBox.Text));
+            if (aRefrigeretor.Validation())
+            {
+                currentWeightTextBox.Text = aRefrigeretor.CurrentWeight().ToString();
+                remainingWeightTextBox.Text = aRefrigeretor.RemainingWeight().ToString();
+            }
+            else
+            {
+                MessageBox.Show("Maximum weight limit exceed");
+            }
+            
+        }
+
+        private void totalButton_Click(object sender, EventArgs e)
+        { double ress;
+            double res1;
+            double res2;
+
+            res1 = Convert.ToDouble(aRefrigeretor.totalaitem().ToString());
+            res2 = Convert.ToDouble(aRefrigeretor.totalunitWeight().ToString());
+            //MessageBox.Show("Number Of Item"+ res1 + ""+ "Number Of Item" + res2);
+
+            totalRichTextBox.Text ="Total Item:" +" "+res1.ToString()+ Environment.NewLine + "Weight: " + " " + res2.ToString()+ Environment.NewLine+ "Total Weight :" +" "+ aRefrigeretor.CurrentWeight().ToString();
+        }
+    }
+}
---
+++ RefrigeratorAppPractice3/RefrigeratorAppPractice3/Refrigeretor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    public class Refrigeretor
+    {
+        private double maxWeight;
+        public double currentWeight = 0;
+        private List<double> itemNo = new List<double>();
+        private List<double> unitWeight = new List<double>();
+        
+        public double MaxWeight { set; get; }
+        public double ItemNo { set; get; }
+        public double UnitWeight { set; get; }
+        public void AddWeight(double maxWeight)
+        {
+            this.maxWeight = maxWeight;
+        }
+        public void AddItemUnit(double itemNo, double unitWeight)
+        {
+            this.itemNo.Add(itemNo);
+            this.unitWeight.Add(unitWeight);
+            this.currentWeight += itemNo * unitWeight;
+        }
+        public double CurrentWeight()
+        {
+            //double currentWeight = this.currentWeight;
+            return currentWeight;
+        }
+        public double RemainingWeight()
+        {
+            double remainingtWeight = this.maxWeight - this.currentWeight;
+            return remainingtWeight;
+        }
+        public bool Validation()
+        {
+            bool validity = true;
+            if (CurrentWeight() > maxWeight)
+            {
+                validity = false;
+            }
+            return validity;
+        }
+        public double totalaitem()
+        {
+           double res=itemNo.Sum();
+            return res;
+        }
+        public double totalunitWeight()
+        {
+            double res1 = unitWeight.Sum();
+            return res1;
+        }
+         
+    }
+}
